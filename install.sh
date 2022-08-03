91d9399e11bce5f21c77f877b4c5f4d007618f6e
harish <110367352+coding9787@users.noreply.github.com>
2022-08-03 11:47:22 +0530

    Install tts

---
+++ install.sh
@@ -0,0 +1,11 @@
+apt update
+apt upgrade
+pkg update
+pkg upgrade
+pkg install git
+pkg install python
+pip install mpv
+pip install gtts
+git clone https://github.com/coding9787/Text-to-speech-termux.git
+cd Text-to-speech-termux
+python tts1.py
